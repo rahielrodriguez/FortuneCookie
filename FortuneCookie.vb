500c77a181c6a0e4795ff6e6f0cb118432f0df19
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-02-04 19:36:32 -0700

    Add project files.

---
+++ FortuneCookie.vb
@@ -0,0 +1,28 @@
+﻿Option Strict On
+Option Explicit On
+
+Module FortuneCookie
+
+    Sub Main()
+        Dim wisdoms As New List(Of String)
+
+        wisdoms.Add("Marriage lets you annoy one special person for the rest of your life.")
+        wisdoms.Add("Lucky is coming your way!")
+        wisdoms.Add("Your life is what your thoughts make it.")
+        wisdoms.Add("You will live to eat many fortune cookies.")
+        wisdoms.Add("Happiness lies in the joy of achievement and the thrill of creative effort.")
+        wisdoms.Add("Curiosity kills boredom. Nothing can kill curiosity.")
+        wisdoms.Add("Keep up the good work. You will be rewarded.")
+        wisdoms.Add("Never fear shadows. They simply mean there's a light shining somewhere nearby.")
+        wisdoms.Add("If you don't fail now and again, it's a sign that you're playing it safe.")
+        wisdoms.Add("Help! I'm being held prisoner in a Chinese bakery!")
+
+        Static random As New Random()
+        Dim randomWisdom = wisdoms(random.Next(wisdoms.Count))
+        Console.WriteLine(randomWisdom)
+
+        Console.Read()
+
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FortuneCookie")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("FortuneCookie")>
+<Assembly: AssemblyCopyright("Copyright © HP 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("45102b3f-9798-4455-bb98-4bf86343a01e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
